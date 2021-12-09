b82922c20b68c6695342c17f7e8b4115cab7262b
eightwoods <45482848+eightwoods@users.noreply.github.com>
2021-12-09 00:24:45 +0000

    Update expire.py

--- trading/expire.py
+++ trading/expire.py
@@ -6,15 +6,15 @@ import os
 from datetime import datetime
 infura_url = r'https://polygon-mainnet.infura.io/v3/' + os.environ['INFURA_API_KEY']
 chain_id = 137
-addresses = ['0x52ea476C538165F0eaE93A2FB1a4FB6D2B8EdFd8',
+addresses = ['0xFC6210087541eF7cd69d32Ee20EC6b3C21918F2F',
              '0x4980D84ca1E4Eee41cf8685bE38359F659d37B89']  # for exchange address
 
 web3 = Web3(Web3.HTTPProvider(infura_url))
 web3.eth.defaultAccount = Account.from_key(os.environ['MNEMONIC']).address
 
 def contract(ad, filename):
-    #return web3.eth.contract(address=ad, abi=read_abi(os.path.join(os.getcwd(), r'Documents/GitHub/moret/build/contracts', filename)))
-    return web3.eth.contract(address=ad, abi=read_abi(os.path.join(r'../build/contracts', filename)))
+    return web3.eth.contract(address=ad, abi=read_abi(os.path.join(os.getcwd(), r'Documents/GitHub/moret/build/contracts', filename)))
+    #return web3.eth.contract(address=ad, abi=read_abi(os.path.join(r'../build/contracts', filename)))
 
 for address in addresses:
     exchange = contract(address, 'Exchange.json')
@@ -23,7 +23,7 @@ for address in addresses:
 
     any_expiring = vault.functions.anyOptionExpiring().call()
     if any_expiring:
-        print("options expiring {} at {}".format(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), address))
+        print("options expiring {} at {}".format(datetime.now().strftime("%d/%m/%Y, %H:%M:%S"), market.address))
         nonce = web3.eth.get_transaction_count(web3.eth.default_account)
         expire_txn = market.functions.expireOptions(web3.eth.default_account).buildTransaction({'gas': 5000000, 'from': web3.eth.default_account, 'nonce': nonce, 'chainId': chain_id, 'gasPrice': web3.toWei(100, 'gwei')})
         #print(expire_txn)
